d343136ace70397c73d354e5f6d90c833f7f0454
peterfalk2 <106140224+peterfalk2@users.noreply.github.com>
2022-07-22 14:34:55 +1000

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement(`h1`);
+newHeader.setAttribute(`id`,`victory`);
+console.log(newHeader.id);
+
+newHeader.innerHTML = `<h1> Peter is the champion </h1>`
